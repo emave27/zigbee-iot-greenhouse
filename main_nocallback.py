af523fc624ea8fa94c8d760af829c3f68d24bdf1
emave27 <48360360+emave27@users.noreply.github.com>
2022-04-01 12:32:51 +0200

    Update main_nocallback.py
    Added discover_remote_device() function to automatically discover the nodes in the network

--- main_nocallback.py
+++ main_nocallback.py
@@ -9,7 +9,7 @@ from digi.xbee.devices import XBeeDevice
 from digi.xbee.io import IOLine, IOMode
 from time import sleep
 
-from digi.xbee.models.address import XBee64BitAddress
+from digi.xbee.models.status import NetworkDiscoveryStatus
 
 MIN=360
 MAX=1023
@@ -26,7 +26,9 @@ LINE_TWO=IOLine.DIO1_AD1
 DIGITAL_OUT_ZERO=IOLine.DIO0_AD0
 DIGITAL_OUT_ONE=IOLine.DIO1_AD1
 
-SAMPLING_RATE=15 #sampling rate in seconds
+#sampling rate in seconds
+#deprecated, use upload_interval
+SAMPLING_RATE=15
 
 first_remote=second_remote=third_remote=None
 threshold=20
@@ -34,8 +36,10 @@ duty_time=5
 upload_interval=15
 mode=True
 fan_ison=False
-pump_ison=False
 upload=False
+#internal bool var to check if pump is running (auto mode)
+#set to False to let the pump run on code start-up
+pump_duty=True
 
 local_device=XBeeDevice(SERIAL_PORT, BAUD_RATE)
 
@@ -55,26 +59,54 @@ def check_temp(temp_c, fan_stat):
         print("Fan is off")
 
 def pump_callback(d_t):
-    global pump_ison
-    if pump_ison:
+    global pump_duty
+    if pump_duty:
         print('Water pump is already running!')
     else:
-        pump_ison=True
+        pump_duty=True
         third_remote.set_dio_value(DIGITAL_OUT_ONE, IOMode.DIGITAL_OUT_LOW)
         sleep(d_t)
         third_remote.set_dio_value(DIGITAL_OUT_ONE, IOMode.DIGITAL_OUT_HIGH)
         print('Water pump off after {0} seconds'.format(d_t))
-        pump_ison=False
+        pump_duty=False
 
-def get_remote_device():
+def discover_remote_device(network, device):
+    device_list=[]
+
+    try:
+        network.set_deep_discovery_timeouts(20)
+        network.clear()
+
+        def callback_device_found(remote):
+            print('Device discovered: %s' % remote)
+            device_list.append(remote.get_node_id())
+
+        def callback_discovery_over(status):
+            if status==NetworkDiscoveryStatus.SUCCESS:
+                print('Discovery process finished successfully')
+            else:
+                print('Error: %s' % status.description)
+        
+        network.add_device_discovered_callback(callback_device_found)
+        network.add_discovery_process_finished_callback(callback_discovery_over)
+
+        network.start_discovery_process()
+
+        while network.is_discovery_running():
+            sleep(1)
+    finally:
+        if device is not None and device.is_open():
+            print('All discovered nodes: ', sorted(device_list))
+            return sorted(device_list)
+
+def connect_remote_device(network, devices):
     global first_remote, second_remote, third_remote
-    xbee_network=local_device.get_network()
 
-    print("Discovering device")
+    print("Connecting device")
 
-    first_remote=xbee_network.discover_device(REMOTE_NODE_IDS[0])
-    second_remote=xbee_network.discover_device(REMOTE_NODE_IDS[1])
-    third_remote=xbee_network.discover_device(REMOTE_NODE_IDS[2])
+    first_remote=network.discover_device(devices[0])
+    second_remote=network.discover_device(devices[1])
+    third_remote=network.discover_device(devices[2])
 
     remote_devices=[first_remote, second_remote, third_remote]
 
@@ -84,16 +116,14 @@ def get_remote_device():
 
     first_remote.set_dest_address(local_device.get_64bit_addr())
     first_remote.set_io_configuration(LINE_ONE, IOMode.ADC)
-    #first_remote.set_io_sampling_rate(SAMPLING_RATE)
 
     second_remote.set_dest_address(local_device.get_64bit_addr())
     second_remote.set_io_configuration(LINE_TWO, IOMode.ADC)
-    #second_remote.set_io_sampling_rate(SAMPLING_RATE)
 
     third_remote.set_io_configuration(DIGITAL_OUT_ZERO, IOMode.DIGITAL_OUT_HIGH)
 
     for rem_dev in remote_devices:
-        print("Device found: ", rem_dev)
+        print("Device connected: ", rem_dev)
 
 def get_data():
     today=str(date.today())
@@ -110,7 +140,7 @@ def get_data():
     check_temp(temp_c, fan_ison)
     functions.upload_data_single(today, current_time, Decimal(temp_c), Decimal(humidity), upload)
 
-    if hum_converted < 50 and pump_ison==False:
+    if hum_converted < 50 and pump_duty==False:
         pump_thread = threading.Thread(target=pump_callback, args=(duty_time,))
         pump_thread.start()
         print('Water pump (should be) running')
@@ -121,8 +151,11 @@ try:
     print("Read and upload data")
     local_device.open()
     local_device.flush_queues()
+    zigbee_network=local_device.get_network()
     print(local_device.get_protocol())
-    get_remote_device()
+
+    discovered_devices=discover_remote_device(zigbee_network, local_device)
+    connect_remote_device(zigbee_network, discovered_devices)
 
     while True:
         threshold, duty_time, upload_interval, mode, _, _, upload=functions.get_settings() 
