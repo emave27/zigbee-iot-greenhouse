7df25ba0f3ed48d14b73f20c1a6a07c6ea4736e4
emave27 <48360360+emave27@users.noreply.github.com>
2022-04-01 10:19:39 +0200

    Update functions.py
    Added upload_data_single() function to handle data upload to a single DynamoDB table. Added get_settings() function to handle settings (settings are updated by the web dashboard interface).

--- functions.py
+++ functions.py
@@ -1,11 +1,49 @@
 import boto3
+from botocore.exceptions import ClientError
 
 dynamodb=None
 if not dynamodb:
     dynamodb=boto3.resource('dynamodb', region_name='')
 
-def upload_data_single(date, time, temp, hum):
-    print("todo")
+def upload_data_single(date, time, temp, hum, upload):
+    if upload:
+        try:
+            table=dynamodb.Table('GreenhouseData')
+            
+            response=table.put_item(
+                Item={
+                    'date': date,
+                    'time': time,
+                    'data':{'temperature': temp,
+                            'humidity': hum}
+                }
+            )
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+        else:
+            print("Data uploaded")
+    else:
+        print("Upload inhibited")
+        
+def get_settings():
+    table=dynamodb.Table('settings')
+
+    try:
+        response=table.get_item(Key={'user_id': '', 'set_id':})
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        res=response['Item']
+    
+    temp_t=int(res['threshold'])
+    d_time=int(res['duty_time'])
+    upd_time=int(res['update'])
+    mode=res['mode']
+    fan=res['fan']
+    pump=res['pump']
+    upload=res['upload']
+
+    return temp_t, d_time, upd_time, mode, fan, pump, upload
 
 def upload_data(date, time, to_upload, is_temp):
     if is_temp:
